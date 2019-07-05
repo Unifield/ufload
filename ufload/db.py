273c06baf66f708ffe0bdfb3bfd77ba7706748d7
asantos <alexandre.santos@geneva.msf.org>
2019-07-05 09:13:59 +0200

    0.247 : disable auto imports/exports (except with -live arg)

--- ufload/db.py
+++ ufload/db.py
@@ -370,7 +370,7 @@ def delive(args, db):
     if rc != 0:
         return rc
     # Automated import settings
-    rc = psql(args, 'TRUNCATE automated_import;', db)
+    rc = psql(args, 'UPDATE automated_import SET report_path=\'\', src_path=\'\', ftp_url=\'\', dest_path=\'\', ftp_ok=\'f\', ftp_port=\'\',dest_path_failure=\'\', ftp_login=\'\', ftp_password=\'\', ftp_protocol=\'\';', db)
     if rc != 0:
         return rc
     # Automated export jobs
@@ -378,7 +378,7 @@ def delive(args, db):
     if rc != 0:
         return rc
     # Automated export settings
-    rc = psql(args, 'TRUNCATE automated_export;', db)
+    rc = psql(args, 'UPDATE automated_export SET report_path=\'\', src_path=\'\', ftp_url=\'\', dest_path=\'\', ftp_ok=\'f\', ftp_port=\'\',dest_path_failure=\'\', ftp_login=\'\', ftp_password=\'\', ftp_protocol=\'\';', db)
     if rc != 0:
         return rc
 
