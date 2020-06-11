de47014490ee139d838e3f6032a04460973708db
Kharianne <katerinamrkackova@gmail.com>
2020-06-11 16:29:08 +0200

    update version

--- ufload/__init__.py
+++ ufload/__init__.py
@@ -2,7 +2,7 @@ import cloud; assert cloud
 import db; assert db
 import httpfile; assert httpfile
 
-__version__ = '0.271'
+__version__ = '0.272'
 
 # null progress, can be overridden by importers
 def _progress(p):
