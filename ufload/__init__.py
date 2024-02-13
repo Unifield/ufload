7e6cc61aade9b545202c50fcbb05bc806a8968b9
jftempo <jfb@tempo-consulting.fr>
2024-02-13 11:22:41 +0100

    Add version 0.279

--- ufload/__init__.py
+++ ufload/__init__.py
@@ -2,7 +2,7 @@ import cloud; assert cloud
 import db; assert db
 import httpfile; assert httpfile
 
-__version__ = '0.278'
+__version__ = '0.279'
 
 # null progress, can be overridden by importers
 def _progress(p):
